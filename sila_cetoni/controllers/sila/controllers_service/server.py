1065f37cf05a3cd53c037b51f068ce9b6231e483
Florian Meinicke <florian.meinicke@cetoni.de>
2022-10-04 15:23:39 +0200

    Fix typo in pyproject.toml, update CHANGELOG-md and bump version to v1.7.1

--- sila_cetoni/controllers/sila/controllers_service/server.py
+++ sila_cetoni/controllers/sila/controllers_service/server.py
@@ -9,7 +9,7 @@ from sila2.server import SilaServer
 from .feature_implementations.controlloopservice_impl import ControlLoopServiceImpl
 from .generated.controlloopservice import ControlLoopServiceFeature
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 
 class Server(SilaServer):
